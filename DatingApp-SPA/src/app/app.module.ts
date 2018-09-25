69f780b15cb4e80e35d36a7ef12c7ac64a5cbebb
mayura <mayuraselvarajah26@gmail.com>
2018-09-24 23:26:15 -0900

    changes upto this

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import {HttpClientModule} from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
 
 
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
